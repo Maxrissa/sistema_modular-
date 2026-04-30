cfa7cc425402d3f2210f0ecc076e0aee88cb4c6d
bapatata688 <b0ldm6diego@gmail.com>
2026-04-29 19:45:56 -0600

    estructura de trabajo| clase padre

--- src/reportes.py
+++ src/reportes.py
@@ -1,4 +1,5 @@
-#
+# aca solo se llaman las funciones declaradas en sus reportes
+# con el self.encuestados
 class AnalizarEncuestados:
     def __init__(self, lista_objetos):
         self.encuestados = lista_objetos
