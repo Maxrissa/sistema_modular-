95dc8873d86fcdd513ec5856907fc122c4a27305
bapatata688 <b0ldm6diego@gmail.com>
2026-04-30 14:37:10 -0600

    trabajo diego escobar

--- src/reportes.py
+++ src/reportes.py
@@ -1,5 +1,28 @@
 # aca solo se llaman las funciones declaradas en sus reportes
-# con el self.encuestados
+from src.reporte_a import (
+    total_encuestados,
+    comida_mas_preferida,
+    encuestados_por_comida,
+    frecuencia_consumo_comun,
+    promedio_gasto_general,
+)
+
+
 class AnalizarEncuestados:
     def __init__(self, lista_objetos):
         self.encuestados = lista_objetos
+
+    def total_encuestados(self):
+        total_encuestados(self.encuestados)
+
+    def comida_mas_preferida(self):
+        comida_mas_preferida(self.encuestados)
+
+    def encuestados_por_comida(self):
+        encuestados_por_comida(self.encuestados)
+
+    def frecuencia_consumo_comun(self):
+        frecuencia_consumo_comun(self.encuestados)
+
+    def promedio_gasto_general(self):
+        promedio_gasto_general(self.encuestados)
